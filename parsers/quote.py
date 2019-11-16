5b7382e7dc965bef4e1253c6cbcdd0ef0abceaa3
un4ckn0wl3z <slickerth@gmail.com>
2019-11-16 23:08:57 +0700

    add parsing content

--- parsers/quote.py
+++ parsers/quote.py
@@ -8,3 +8,8 @@ class QuoteParser:
 
     def __init__(self, parent):
         self.parent = parent
+
+    @property
+    def content(self):
+        locator = QuoteLocators.CONTENT
+        self.parent.select_one(locator).string
