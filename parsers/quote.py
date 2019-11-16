ba6c77c68ae5ac363751edc8b982e64186257d9b
un4ckn0wl3z <slickerth@gmail.com>
2019-11-16 23:15:07 +0700

    complete QuoteParser

--- parsers/quote.py
+++ parsers/quote.py
@@ -9,7 +9,20 @@ class QuoteParser:
     def __init__(self, parent):
         self.parent = parent
 
+    def __repr__(self):
+        return f'<Quote {self.content}, by {self.author}>'
+
     @property
     def content(self):
         locator = QuoteLocators.CONTENT
-        self.parent.select_one(locator).string
+        return self.parent.select_one(locator).string
+
+    @property
+    def author(self):
+        locator = QuoteLocators.AUTHOR
+        return self.parent.select_one(locator).string
+
+    @property
+    def tags(self):
+        locator = QuoteLocators.TAGS
+        return [e.string for e in self.parent.select_one(locator)]
