a3d6f0789ac760e30259d0fbf694077858649f81
un4ckn0wl3z <slickerth@gmail.com>
2019-11-24 19:00:38 +0700

    complete selenuim feature

--- locators/quote_locators.py
+++ locators/quote_locators.py
@@ -1,4 +1,4 @@
 class QuoteLocators:
-    AUTHOR = 'small.author'
-    CONTENT = 'span.text'
-    TAGS = 'div.tags a.tag'
+    AUTHOR = 'span.author'
+    CONTENT = 'span.content'
+    TAGS = 'span.tag'
--- pages/quotes_page.py
+++ pages/quotes_page.py
@@ -1,5 +1,5 @@
-
-
+from typing import List
+from selenium.webdriver.support.ui import Select
 from locators.quotes_page_locators import QuotesPageLocators
 from parsers.quote import QuoteParser
 
@@ -8,11 +8,41 @@ class QuotesPage:
     """
     Init
     """
+
     def __init__(self, browser):
         self.browser = browser
 
     @property
-    def quotes(self):
+    def quotes(self) -> List[QuoteParser]:
         locator = QuotesPageLocators.QUOTE
         quotes_tags = self.browser.find_elements_by_css_selector(locator)
         return [QuoteParser(e) for e in quotes_tags]
+
+    @property
+    def author_dropdown(self) -> Select:
+        element = self.browser.find_element_by_css_selector(
+            QuotesPageLocators.AUTHOR_DROPDOWN
+        )
+        return Select(element)
+
+    @property
+    def tags_dropdown(self) -> Select:
+        element = self.browser.find_element_by_css_selector(
+            QuotesPageLocators.TAG_DROPDOWN
+        )
+        return Select(element)
+
+    @property
+    def search_button(self):
+        return self.browser.find_element_by_css_selector(
+            QuotesPageLocators.SEARCH_BUTTON
+        )
+
+    def select_author(self, author_name: str):
+        self.author_dropdown.select_by_visible_text(author_name)
+
+    def get_available_tags(self) -> List[str]:
+        return [option.text.strip() for option in self.tags_dropdown.options]
+
+    def select_tag(self, selected_tag: str):
+        self.tags_dropdown.select_by_visible_text(selected_tag)
